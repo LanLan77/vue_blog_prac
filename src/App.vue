e70cbc1aeab70ab1313f397c368d20b748c8b10a
LanLan77 <775172380@qq.com>
2023-09-26 16:50:52 +0930

    路由

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>
+		<router-link to="/foo">Go to Foo</router-link>
+		<br/>
+		<router-link to="/bar">Go to Bar</router-link>
+		<br/>
+		<router-link to="/user/bbbb">Go to User</router-link>
+	</p>
+	<router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+     
   }
 }
 </script>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>Bar</div>' }
+const User = {
+  template: '<div>User{{ $route.params.id }}</div>'
+}
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar },
+  { path: '/user/:id', component: User }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
