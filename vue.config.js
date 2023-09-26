29842b30f1c77b3458a3d914a2dd2ef1329513cc
LanLan77 <775172380@qq.com>
2023-09-26 17:05:09 +0930

    fix index.html

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  runtimeCompiler: true
+  runtimeCompiler: true,
+  // 其他配置...
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].template = './index.html'; // 实际的相对路径
+      return args;
+    });
+  },
 };
